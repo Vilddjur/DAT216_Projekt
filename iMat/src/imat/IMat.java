81b0402b2ec73f8c9dd63f70bb1fb0e6c56688d3
oskar367 <oskar.jedvert@gmail.com>
2015-02-09 12:16:25 +0100

    first commit

--- iMat/src/imat/IMat.java
+++ iMat/src/imat/IMat.java
@@ -16,6 +16,7 @@ public class IMat {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Starting iMat...");
     }
     
 }
