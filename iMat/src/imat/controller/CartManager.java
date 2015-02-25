4971ebbb6941b87f7fd89d0e0ca89f4e5eec5332
Sarar Innab <innab@net.chalmers.se>
2015-02-25 10:15:40 +0100

    added manager classes without code
    Added manager classes. They are supposed to work as controllers

---
+++ iMat/src/imat/controller/CartManager.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imat.controller;
+
+import se.chalmers.ait.dat215.project.ShoppingCart;
+
+/**
+ *
+ * @author win8
+ */
+public class CartManager {
+    ShoppingCart cart;
+    
+    
+    
+}
