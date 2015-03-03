d9e981809dc0994e97f5a0f8fae42c8e999f8c90
sCarlman <carl.albertsson@gmail.com>
2015-03-03 16:40:00 +0100

    Changes colors in Constants

--- iMat/src/imat/Constants.java
+++ iMat/src/imat/Constants.java
@@ -12,13 +12,21 @@ import java.awt.Color;
  * @author mats
  */
 public final class Constants {
-    public static final Color DEFAULT_COLOR = Color.white;
-    public static final Color HOVER_COLOR = new Color(246, 246, 246);
-    public static final Color PRESSED_COLOR = new Color(237, 237, 237);
+    public static final Color DEFAULT_COLOR = new Color(0x7ec55d);
+    public static final Color HOVER_COLOR = new Color(0xaaed93);
+    public static final Color PRESSED_COLOR = new Color(0xd8fec6);
     
-    public static final Color HIGHLIGHT_COLOR = new Color(255, 251, 10);
-    public static final Color HIGHLIGHT_HOVER_COLOR = new Color(255, 238, 5);
+    //public static final Color DEFAULT_COLOR = Color.white;
+    //public static final Color HOVER_COLOR = new Color(246, 246, 246);
+    //public static final Color PRESSED_COLOR = new Color(237, 237, 237);
+    
+    public static final Color HIGHLIGHT_COLOR = new Color(0x666666);
+    public static final Color HIGHLIGHT_HOVER_COLOR = new Color(0x767676);
     public static final Color HIGHLIGHT_PRESSED_COLOR = new Color(255, 219, 15);
+    
+    //public static final Color HIGHLIGHT_COLOR = new Color(255, 251, 10);
+    //public static final Color HIGHLIGHT_HOVER_COLOR = new Color(255, 238, 5);
+    //public static final Color HIGHLIGHT_PRESSED_COLOR = new Color(255, 219, 15);
     
     public static final Color ERROR_COLOR = Color.RED;
     
