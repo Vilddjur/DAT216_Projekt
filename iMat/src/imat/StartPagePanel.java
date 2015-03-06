19a535c90023424894a244c3232b40a906fe0010
sCarlman <carl.albertsson@gmail.com>
2015-03-06 12:08:46 +0100

    change gap in StartPage

--- iMat/src/imat/StartPagePanel.java
+++ iMat/src/imat/StartPagePanel.java
@@ -74,7 +74,7 @@ public class StartPagePanel extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addGap(10, 10, 10)
                 .addComponent(slidePanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, 0)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
