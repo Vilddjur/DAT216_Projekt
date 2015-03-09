68643f89d5b01d5c00c48937b17c0290c7d6fab2
Mats Högberg <mats@hgbrg.se>
2015-03-09 15:41:03 +0100

    Moved all field validations to a separate class

---
+++ iMat/src/imat/FieldValidations.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imat;
+
+import java.util.Calendar;
+import static java.util.Calendar.DAY_OF_YEAR;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+/**
+ *
+ * @author mats
+ */
+public class FieldValidations {
+    private FieldValidations() {
+        throw new AssertionError();
+    }
+    
+    public static boolean isValidEmail(String email) {
+        if (email.equals("")) {
+            return false;
+        } else {
+            boolean result = true;
+            try {
+               InternetAddress emailAddr = new InternetAddress(email);
+               emailAddr.validate();
+            } catch (AddressException ex) {
+               result = false;
+            }
+            return result;
+        }
+    }
+    
+    public static boolean isValidPersnbr(String persnbr) {
+        String yearStr;
+        String monthStr;
+        String dayStr;
+        String secretStr;
+        
+        if (persnbr.indexOf("-") != -1 && persnbr.indexOf("-") != persnbr.length() - 5) {
+            return false;
+        }
+        
+        persnbr = persnbr.replaceFirst("-", "");
+        
+        if (persnbr.length() == 10) {
+            yearStr = persnbr.substring(0, 2);
+            persnbr = persnbr.substring(2);
+        } else if (persnbr.length() == 12) {
+            yearStr = persnbr.substring(0, 4);
+            persnbr = persnbr.substring(4);
+        } else {
+            return false;
+        }
+        
+        monthStr = persnbr.substring(0, 2);
+        dayStr = persnbr.substring(2, 4);
+        secretStr = persnbr.substring(4, 8);
+        
+        int year;
+        int month;
+        int day;
+        int secret;
+
+        try {
+            year = Integer.parseInt(yearStr);
+            month = Integer.parseInt(monthStr);
+            day = Integer.parseInt(dayStr);
+            secret = Integer.parseInt(secretStr);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        
+        if (year > 0
+                && month >= 1 && month <= 12
+                && day >= 1 && day <= daysInMonth(month, year)) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public static boolean isValidPostCode(String postCode) {
+        if (postCode.indexOf(" ") != -1 && postCode.indexOf(" ") != postCode.length() - 3) {
+            return false;
+        }
+        
+        postCode = postCode.replaceFirst(" ", "");
+        
+        if (postCode.length() != 5) {
+            return false;
+        }
+        
+        try {
+            int i = Integer.parseInt(postCode);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+    
+    public static boolean isValidPhoneNumber(String number) {
+        return number.matches("^(\\+?46|0)7\\d{8}$");
+    }
+    
+    public static int daysInMonth(int m, int y) {
+        switch (m) {
+            case 2:
+                if (isLeapYear(y)) {
+                    return 29;
+                } else {
+                    return 28;
+                }
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                return 31;
+            case 4: case 6: case 9: case 11:
+                return 30;
+        }
+        return 0;
+    }
+    
+    public static boolean isLeapYear(int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        return cal.getActualMaximum(DAY_OF_YEAR) > 365;
+    }
+}
--- iMat/src/imat/RegisterPanel.java
+++ iMat/src/imat/RegisterPanel.java
@@ -339,28 +339,28 @@ public class RegisterPanel extends javax.swing.JPanel {
     private boolean validateMandatoryFields() {
         boolean error = false;
         
-        if (!isValidEmail(emailField.getText())) {
+        if (!FieldValidations.isValidEmail(emailField.getText())) {
             emailLabel.setForeground(Constants.ERROR_COLOR);
             emailStarLabel.setForeground(Constants.ERROR_COLOR);
             error = true;
         }
         
         if (!persnbrField.getText().equals("")) {
-            if (!isValidPersnbr(persnbrField.getText())) {
+            if (!FieldValidations.isValidPersnbr(persnbrField.getText())) {
                 persnbrLabel.setForeground(Constants.ERROR_COLOR);
                 error = true;
             }
         }
         
         if (!postCodeField.getText().equals("")) {
-            if (!isValidPostCode(postCodeField.getText())) {
+            if (!FieldValidations.isValidPostCode(postCodeField.getText())) {
                 postCodeLabel.setForeground(Constants.ERROR_COLOR);
                 error = true;
             }
         }
         
         if (!phoneField.getText().equals("")) {
-            if (!isValidPhoneNumber(phoneField.getText())) {
+            if (!FieldValidations.isValidPhoneNumber(phoneField.getText())) {
                 phoneLabel.setForeground(Constants.ERROR_COLOR);
                 error = true;
             }
@@ -380,115 +380,6 @@ public class RegisterPanel extends javax.swing.JPanel {
         }
         
         return error;
-    }
-    
-    private static boolean isValidEmail(String email) {
-        if (email.equals("")) {
-            return false;
-        } else {
-            boolean result = true;
-            try {
-               InternetAddress emailAddr = new InternetAddress(email);
-               emailAddr.validate();
-            } catch (AddressException ex) {
-               result = false;
-            }
-            return result;
-        }
-    }
-    
-    private static boolean isValidPersnbr(String persnbr) {
-        String yearStr;
-        String monthStr;
-        String dayStr;
-        String secretStr;
-        
-        if (persnbr.indexOf("-") != -1 && persnbr.indexOf("-") != persnbr.length() - 5) {
-            return false;
-        }
-        
-        persnbr = persnbr.replaceFirst("-", "");
-        
-        if (persnbr.length() == 10) {
-            yearStr = persnbr.substring(0, 2);
-            persnbr = persnbr.substring(2);
-        } else if (persnbr.length() == 12) {
-            yearStr = persnbr.substring(0, 4);
-            persnbr = persnbr.substring(4);
-        } else {
-            return false;
-        }
-        
-        monthStr = persnbr.substring(0, 2);
-        dayStr = persnbr.substring(2, 4);
-        secretStr = persnbr.substring(4, 8);
-        
-        int year;
-        int month;
-        int day;
-        int secret;
-
-        try {
-            year = Integer.parseInt(yearStr);
-            month = Integer.parseInt(monthStr);
-            day = Integer.parseInt(dayStr);
-            secret = Integer.parseInt(secretStr);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        
-        if (year > 0
-                && month >= 1 && month <= 12
-                && day >= 1 && day <= daysInMonth(month, year)) {
-            return true;
-        }
-        
-        return false;
-    }
-    
-    private static boolean isValidPostCode(String postCode) {
-        if (postCode.indexOf(" ") != -1 && postCode.indexOf(" ") != postCode.length() - 3) {
-            return false;
-        }
-        
-        postCode = postCode.replaceFirst(" ", "");
-        
-        if (postCode.length() != 5) {
-            return false;
-        }
-        
-        try {
-            int i = Integer.parseInt(postCode);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
-    }
-    
-    private static boolean isValidPhoneNumber(String number) {
-        return number.matches("^(\\+?46|0)7\\d{8}$");
-    }
-    
-    private static int daysInMonth(int m, int y) {
-        switch (m) {
-            case 2:
-                if (isLeapYear(y)) {
-                    return 29;
-                } else {
-                    return 28;
-                }
-            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                return 31;
-            case 4: case 6: case 9: case 11:
-                return 30;
-        }
-        return 0;
-    }
-    
-    public static boolean isLeapYear(int year) {
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, year);
-        return cal.getActualMaximum(DAY_OF_YEAR) > 365;
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
